7e1602b69ee263226c71b23a93513b6605f7bb75
rubel-sh <rubel.hossain0619@gmail.com>
2023-04-08 13:24:12 +0600

    [fixed] warnings

--- context/AuthProvider.js
+++ context/AuthProvider.js
@@ -3,7 +3,7 @@ import { createContext } from "react";
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-    return <AuthContext.Provider>{children}</AuthContext.Provider>;
+    return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
 };
 
 export default AuthProvider;
